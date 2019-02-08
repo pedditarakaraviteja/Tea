bbfaae1d100f4b338f17e2e9e3d3fb773c8245cd
pedditarakaraviteja <pedditarakaraviteja@gmail.com>
2019-02-08 15:34:45 -0600

    hh

--- Tea/tests/UnitTests/UnitTest1.cs
+++ Tea/tests/UnitTests/UnitTest1.cs
@@ -11,4 +11,44 @@ namespace UnitTests
 
         }
     }
+    public class Calculator
+    {
+        public int Add(int value1, int value2)
+        {
+            return value1 + value2;
+        }
+    }
+    public class CalculatorTests
+    {
+        [Fact]
+        public void CanAdd()
+        {
+            var calculator = new Calculator();
+
+            int value1 = 1;
+            int value2 = 2;
+
+            var result = calculator.Add(value1, value2);
+
+            Assert.Equal(3, result);
+        }
+
+
+
+        [Theory]
+        [InlineData(1, 2, 3)]
+        [InlineData(-4, -6, -10)]
+        [InlineData(-2, 2, 0)]
+     
+        public void CanAddTheory(int value1, int value2, int expected)
+        {
+            var calculator = new Calculator();
+
+            var result = calculator.Add(value1, value2);
+
+            Assert.Equal(expected, result);
+        }
+    }
 }
+
+
